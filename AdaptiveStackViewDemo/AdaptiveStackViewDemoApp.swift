066b908570df309c108df4abfb56b3a511a6abc3
Prayag Gediya <prayag.work60@gmail.com>
2023-02-02 10:24:30 +0530

    Initial Commit

---
+++ AdaptiveStackViewDemo/AdaptiveStackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveStackViewDemoApp.swift
+//  AdaptiveStackViewDemo
+//
+//  Created by Prayag Gediya on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveStackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
